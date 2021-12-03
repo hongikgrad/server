4bf1b62195d326b04e600d1ab27cbfb056cd09b0
junhwan <ujunhwan@gmail.com>
2021-12-04 02:48:05 +0900

    [feat] 배포환경 CORS 셋팅

--- src/main/java/com/hongikgrad/HongikGradApplication.java
+++ src/main/java/com/hongikgrad/HongikGradApplication.java
@@ -1,11 +1,8 @@
 package com.hongikgrad;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableJpaAuditing
 @SpringBootApplication
