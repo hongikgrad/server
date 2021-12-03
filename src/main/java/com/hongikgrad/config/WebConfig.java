29c06db1ea381d4290fef807b34fe0e21881f901
Junhwan Yu <ujunhwan@gmail.com>
2021-12-03 14:51:56 +0900

    [feat] local https 설정 (#1)

---
+++ src/main/java/com/hongikgrad/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.hongikgrad.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("https://localhost:8080")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://localhost:3000")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
