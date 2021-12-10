d295a81cd5bb4d0d4e09ca7568038d91831e2034
junhwan <ujunhwan@gmail.com>
2021-12-10 17:55:03 +0900

    [feat] Querydsl QueryFactory 빈 생성

--- src/main/java/com/hongikgrad/HongikGradApplication.java
+++ src/main/java/com/hongikgrad/HongikGradApplication.java
@@ -1,5 +1,6 @@
 package com.hongikgrad;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.persistence.EntityManager;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -39,4 +42,10 @@ public class HongikGradApplication {
 			}
 		};
 	}
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
+	}
+
 }
