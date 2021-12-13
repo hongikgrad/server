5f28ffd11db866943dae7921c126c16ef4bcec87
Junhwan Yu <ujunhwan@gmail.com>
2021-12-13 15:12:47 +0900

    Revert "[feat] 유저 수강 과목 기능, 시간표에서 크롤링 구현 (#9)" (#13)
    This reverts commit 0db9fed19da55389198cfaa2341d0c393d1bb3a7.

--- src/main/java/com/hongikgrad/course/repository/AbeekRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hongikgrad.course.repository;
-
-import com.hongikgrad.course.entity.Abeek;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AbeekRepository extends JpaRepository<Abeek, Long> {
-}
