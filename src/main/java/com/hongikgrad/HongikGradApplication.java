a5e299079285db44c425eb103070f5b4ec3e7679
junhwan <ujunhwan@gmail.com>
2021-12-13 16:44:41 +0900

    [feat] graduation 삭제

--- src/main/java/com/hongikgrad/HongikGradApplication.java
+++ src/main/java/com/hongikgrad/HongikGradApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
