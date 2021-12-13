b514fed68a4f5a4a030cdf4a34f9f0acefd338b5
junhwan <ujunhwan@gmail.com>
2021-12-13 12:48:14 +0900

    [feat] user repository 메서드 추가

--- src/main/java/com/hongikgrad/authentication/repository/UserRepository.java
+++ src/main/java/com/hongikgrad/authentication/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.hongikgrad.authentication.repository;public interface UserRepository {
+package com.hongikgrad.authentication.repository;
+
+import com.hongikgrad.authentication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByStudentId(String studentId);
+    public Boolean existsUserByStudentId(String studentId);
 }
