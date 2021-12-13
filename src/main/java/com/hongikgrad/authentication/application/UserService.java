b7ebfe6895433078ca4db10414c5af86b49b3f69
junhwan <ujunhwan@gmail.com>
2021-12-13 12:47:03 +0900

    [feat] 유저 정보 쿠키에 담는 기능 추가

--- src/main/java/com/hongikgrad/authentication/application/UserService.java
+++ src/main/java/com/hongikgrad/authentication/application/UserService.java
@@ -1,6 +1,10 @@
 package com.hongikgrad.authentication.application;
 
 import com.hongikgrad.authentication.dto.LoginRequestDto;
+import com.hongikgrad.authentication.entity.User;
+import com.hongikgrad.authentication.repository.UserRepository;
+import com.hongikgrad.common.crawler.UserCookieCrawler;
+import com.hongikgrad.common.hash.SHA256;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +12,7 @@ import javax.naming.AuthenticationException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
 @Service
@@ -15,18 +20,28 @@ import java.util.Map;
 public class UserService {
 
     private final UserCookieCrawler userCookieCrawler;
+    private final UserRepository userRepository;
+    private final SHA256 sha256;
 
-    public void login(LoginRequestDto loginDto, HttpServletResponse response) throws IOException, AuthenticationException {
-        Map<String, String> loginInfo = Map.of(
+    public void login(LoginRequestDto loginDto, HttpServletResponse response) throws IOException, AuthenticationException, NoSuchAlgorithmException {
+        Map<String, String> loginData = Map.of(
                 "USER_ID", loginDto.getId(),
                 "PASSWD", loginDto.getPw()
         );
 
-        Map<String, String> userAuthCookie = userCookieCrawler.getUserAuthCookie(loginInfo);
-        setUserCookieResponse(userAuthCookie, response);
+        Map<String, String> userAuthCookie = userCookieCrawler.getUserAuthCookie(loginData);
+        String studentId = sha256.hash(loginDto.getId());
+        setUserAuthCookie(userAuthCookie, response);
+        setUserStudentIdCookie(studentId, response);
+        saveUser(studentId);
     }
 
-    private void setUserCookieResponse(Map<String, String> userAuthCookie, HttpServletResponse response) {
+    private void saveUser(String studentId) {
+        if(userRepository.existsUserByStudentId(studentId)) return;
+        userRepository.save(new User(studentId));
+    }
+
+    private void setUserAuthCookie(Map<String, String> userAuthCookie, HttpServletResponse response) {
         userAuthCookie.forEach((key, value) -> {
             Cookie cookie = new Cookie(key, value);
             cookie.setMaxAge(60*60*24);
@@ -35,5 +50,15 @@ public class UserService {
             cookie.setPath("/");
             response.addCookie(cookie);
         });
+    }
+
+    private void setUserStudentIdCookie(String studentId, HttpServletResponse response) {
+        /* 암호화된 유저 아이디 쿠키에 넣어줌 */
+        Cookie cookie = new Cookie("sid", studentId);
+        cookie.setMaxAge(60*60*24);
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
     }
 }
