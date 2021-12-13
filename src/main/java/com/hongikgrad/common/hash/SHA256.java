3f6b91945f829bba3673f81b815ac87be478a419
junhwan <ujunhwan@gmail.com>
2021-12-13 13:01:21 +0900

    [feat] Hash 인코링 방법 변경

--- src/main/java/com/hongikgrad/common/hash/SHA256.java
+++ src/main/java/com/hongikgrad/common/hash/SHA256.java
@@ -1,19 +1,21 @@
 package com.hongikgrad.common.hash;
 
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+@Component
 public class SHA256 {
 
-    public String Hash(String studentId) throws NoSuchAlgorithmException {
+    public String hash(String studentId) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
-        md.update(studentId.getBytes());
+        md.update(studentId.getBytes(StandardCharsets.UTF_8));
         byte[] bytes = md.digest();
-
-        StringBuilder builder = new StringBuilder();
-        for (byte b : bytes) {
-            builder.append(String.format("%02x", b));
-        }
-        return builder.toString();
+        return String.format("%064x", new BigInteger(1, bytes));
     }
+
 }
