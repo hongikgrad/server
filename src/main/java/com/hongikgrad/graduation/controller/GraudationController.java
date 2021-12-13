211508e0874134517f2d30cb0dc587244f6535e1
junhwan <ujunhwan@gmail.com>
2021-12-13 13:06:39 +0900

    [feat] graduation controller frame 생성

--- src/main/java/com/hongikgrad/graduation/controller/GraudationController.java
+++ src/main/java/com/hongikgrad/graduation/controller/GraudationController.java
@@ -1,2 +1,15 @@
-package com.hongikgrad.graduation.controller;public class GraudationController {
+package com.hongikgrad.graduation.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GraudationController {
+
+    /* 유저가 졸업요건을 만족하는지 여부를 응답 */
+    @GetMapping("users/graduation")
+    public void userGraduation() {
+
+    }
 }
--- src/main/java/com/hongikgrad/graduation/controller/RequirementsCRUDController.java
+++ src/main/java/com/hongikgrad/graduation/controller/RequirementsCRUDController.java
@@ -1,8 +1,28 @@
 package com.hongikgrad.graduation.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class GraduationController {
+public class RequirementsCRUDController {
+
+    @GetMapping
+    public ResponseEntity readRequirements() {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity createRequirement() {
+        return null;
+    }
+
+    @PatchMapping
+    public ResponseEntity updateRequirement() {
+        return null;
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteRequirement() {
+        return null;
+    }
 }
