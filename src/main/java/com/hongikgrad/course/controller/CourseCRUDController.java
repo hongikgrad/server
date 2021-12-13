0e63cfb84ad95f6054b73448c150885c7b098206
junhwan <ujunhwan@gmail.com>
2021-12-13 13:04:45 +0900

    [feat] controller 분리 및 시간표 가져오는 기능 구현

--- src/main/java/com/hongikgrad/course/controller/CourseCRUDController.java
+++ src/main/java/com/hongikgrad/course/controller/CourseCRUDController.java
@@ -1,2 +1,76 @@
-package com.hongikgrad.course.controller;public class CourseCRUDController {
+package com.hongikgrad.course.controller;
+
+import com.hongikgrad.course.application.CourseService;
+import com.hongikgrad.course.dto.SearchCourseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class CourseCRUDController {
+
+    private final CourseService courseService;
+
+    @GetMapping("/courses")
+    public ResponseEntity readCourses() {
+        return null;
+    }
+
+    @PostMapping("/courses/abeek")
+    public ResponseEntity<String> saveAbeekCourses(HttpServletResponse request, @RequestBody SearchCourseDto searchCourseDto) {
+        try {
+            Map<String, String> data = Map.of(
+                    "p_yy", searchCourseDto.getP_yy(),
+                    "p_hakgi", searchCourseDto.getP_hakgi(),
+                    "p_ibhak", "2016",
+                    "p_campus", "1",
+                    "p_gubun", "1",
+                    "p_dept", searchCourseDto.getP_dept(),
+                    "p_grade", searchCourseDto.getP_grade(),
+                    "p_abeek", "1"
+            );
+            courseService.saveAbeekCoursesFromTimeTable(data);
+            return new ResponseEntity<String>("저장 성공", HttpStatus.OK);
+        } catch(IOException | IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/courses/nonabeek")
+    public ResponseEntity<String> saveNonAbeekCourses(HttpServletResponse request, @RequestBody SearchCourseDto searchCourseDto) {
+        try {
+            Map<String, String> data = Map.of(
+                    "p_yy", searchCourseDto.getP_yy(),
+                    "p_hakgi", searchCourseDto.getP_hakgi(),
+                    "p_ibhak", "2016",
+                    "p_campus", "1",
+                    "p_gubun", "1",
+                    "p_dept", searchCourseDto.getP_dept(),
+                    "p_grade", searchCourseDto.getP_grade(),
+                    "p_abeek", "1"
+            );
+            courseService.saveNonAbeekCoursesFromTimeTable(data);
+            return new ResponseEntity<String>("저장 성공", HttpStatus.OK);
+        } catch(IOException | IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping("/courses")
+    public ResponseEntity updateCourses() {
+        return null;
+    }
+
+    @DeleteMapping("/courses")
+    public ResponseEntity deleteCourses() {
+        return null;
+    }
 }
--- src/main/java/com/hongikgrad/course/controller/UserCourseController.java
+++ src/main/java/com/hongikgrad/course/controller/UserCourseController.java
@@ -5,53 +5,43 @@ import com.hongikgrad.course.dto.CourseResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
-public class CourseController {
+public class UserCourseController {
 
     private final CourseService courseService;
 
-    /* 유저가 들은 수업들 응답 */
+    /* 유저가 들은 수업들을 응답 */
     @GetMapping("/users/courses")
-    public ResponseEntity<List<CourseResponseDto>> getUserCourses(HttpServletRequest request) {
+    public ResponseEntity getUserCourses(HttpServletRequest request) {
         try {
             List<CourseResponseDto> userTakenCourses = courseService.getUserTakenCourses(request);
             return new ResponseEntity<List<CourseResponseDto>>(userTakenCourses, HttpStatus.OK);
-        } catch (IOException | NullPointerException e) {
+        } catch (IOException e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (NullPointerException e) {
+            return new ResponseEntity<String>("sid를 알 수 없습니다.", HttpStatus.NOT_FOUND);
         }
     }
 
-    /* 유저가 들은 수업들 데이터베이스에 저장 */
-    @PutMapping("/users/courses")
-    public ResponseEntity putUserCourses(HttpServletRequest request) {
+    /* 유저가 들은 수업들 크롤링 한 뒤 데이터베이스에 저장 */
+    @PostMapping("/users/courses")
+    public ResponseEntity<String> saveUserCourses(HttpServletRequest request) {
         try {
             courseService.saveUserTakenCourses(request);
             return new ResponseEntity<String>("저장 성공", HttpStatus.OK);
-        } catch (IOException | NullPointerException e) {
+        } catch (IOException e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch(NullPointerException e) {
+            return new ResponseEntity<String>("크롤링 실패", HttpStatus.BAD_REQUEST);
         }
-    }
-
-    /* 등록되어있는 수업들 응답 */
-    @GetMapping("/courses")
-    public ResponseEntity getCourses() {
-        return null;
-    }
-
-    /* 수업 저장 */
-    @PostMapping("/courses")
-    public ResponseEntity saveCourses() {
-        return null;
     }
 }
