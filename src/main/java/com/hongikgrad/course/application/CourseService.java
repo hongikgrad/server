31a621fc6e19a8fc83d812628694cb4873e24a7d
junhwan <ujunhwan@gmail.com>
2021-12-16 15:24:56 +0900

    [feat] save 메서드 리팩토링, 등록된 수업 조회 메서드 리팩토링

--- src/main/java/com/hongikgrad/course/application/CourseService.java
+++ src/main/java/com/hongikgrad/course/application/CourseService.java
@@ -2,46 +2,78 @@ package com.hongikgrad.course.application;
 
 import com.hongikgrad.authentication.entity.User;
 import com.hongikgrad.authentication.repository.UserRepository;
+import com.hongikgrad.common.application.CookieService;
 import com.hongikgrad.common.crawler.CourseCrawler;
 import com.hongikgrad.common.crawler.UserCourseCrawler;
+import com.hongikgrad.course.dto.CourseCrawlingDto;
 import com.hongikgrad.course.dto.CourseResponseDto;
+import com.hongikgrad.course.entity.Abeek;
 import com.hongikgrad.course.entity.Course;
+import com.hongikgrad.course.entity.Major;
 import com.hongikgrad.course.entity.UserCourse;
+import com.hongikgrad.course.repository.AbeekRepository;
 import com.hongikgrad.course.repository.CourseRepository;
+import com.hongikgrad.course.repository.MajorRepository;
 import com.hongikgrad.course.repository.UserCourseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class CourseService {
 
-    private final UserCourseCrawler userCourseCrawler;
+    /* 이렇게 많아도 되나...?? */
     private final UserCourseRepository userCourseRepository;
     private final UserRepository userRepository;
     private final CourseRepository courseRepository;
+
+    private final UserCourseCrawler userCourseCrawler;
     private final CourseCrawler courseCrawler;
 
+    private final CookieService cookieService;
+
+    public List<CourseResponseDto> getAllCourses() {
+        List<CourseResponseDto> ret = new ArrayList<>();
+        List<Course> courses = courseRepository.findAll();
+        courses.forEach((course) -> {
+            ret.add(new CourseResponseDto(course.getName(), course.getCredit(), course.getNumber()));
+        });
+        return ret;
+    }
+
     public List<CourseResponseDto> getUserTakenCourses(HttpServletRequest request) throws IOException, NullPointerException{
-        String studentId = getStudentIdFromCookie(request);
+        String studentId = cookieService.getStudentIdFromCookie(request);
         return userCourseRepository.findUserTakenCoursesByStudentId(studentId);
     }
 
     /* 유저가 들은 과목들 클래스넷에서 가져와서 저장 */
     public void saveUserTakenCourses(HttpServletRequest request) throws IOException, NullPointerException {
         List<CourseResponseDto> userTakenCourses = userCourseCrawler.getUserTakenCoursesFromClassnet(request);
-        User user = userRepository.findByStudentId(getStudentIdFromCookie(request));
+        User user = userRepository.findByStudentId(cookieService.getStudentIdFromCookie(request));
         for (CourseResponseDto userTakenCourse : userTakenCourses) {
             Course course = courseRepository.findByNumberAndAndCredit(userTakenCourse.getNumber(), userTakenCourse.getCredit());
             UserCourse userCourse = new UserCourse(user, course);
             userCourseRepository.save(userCourse);
         }
+    }
+
+    public void saveCoursesFromTimeTable(Map<String, String> data) throws IOException {
+        try {
+            List<Course> courses = new ArrayList<>();
+            Set<CourseCrawlingDto> courseDtos = courseCrawler.getCoursesFromTimeTable(data);
+            courseDtos.forEach((course) -> {
+                if (!courseRepository.existsCourseByNumberAndCredit(course.getNumber(), course.getCredit())) {
+                    courses.add(new Course(course.getName(), course.getCredit(), course.getNumber(), course.getAbeek()));
+                }
+            });
+            System.out.println(data.get("p_yy") + data.get("p_hakgi") + data.get("p_dept"));
+            courseRepository.saveAll(courses);
+        } catch(IndexOutOfBoundsException ignored) {}
     }
 
     /* 홍익대 시간표에서 과목을 가져와서 저장 */
@@ -50,6 +82,11 @@ public class CourseService {
         for (Course course : courses) {
             if (courseRepository.existsCourseByNumberAndCredit(course.getNumber(), course.getCredit())) continue;
             courseRepository.save(course);
+        }
+
+        for(Course course : courses) {
+            Course findCourse = courseRepository.findByNumberAndAndCredit(course.getNumber(), course.getCredit());
+//            Abeek abeek = new Abeek(findCourse, );
         }
     }
 
@@ -60,16 +97,4 @@ public class CourseService {
             courseRepository.save(course);
         }
     }
-
-    private String getStudentIdFromCookie(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            String key = cookie.getName();
-            if (key.equals("sid")) {
-                return cookie.getValue();
-            }
-        }
-        throw new NullPointerException();
-    }
-
 }
---
+++ src/main/java/com/hongikgrad/course/dto/InquiredCoursesResponseDto.java
@@ -0,0 +1,8 @@
+package com.hongikgrad.course.dto;
+
+import java.util.List;
+
+public class AllCoursesResponseDto {
+	private int count;
+	private List<CourseResponseDto> courses;
+}
