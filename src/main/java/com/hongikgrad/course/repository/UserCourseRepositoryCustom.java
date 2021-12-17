e978b73d34e2e81d07b3cd6acecc07ed0de8a576
junhwan <ujunhwan@gmail.com>
2021-12-17 18:00:54 +0900

    [feat] CourseDto로 변경, 메서드 추가

--- src/main/java/com/hongikgrad/course/repository/UserCourseRepositoryCustom.java
+++ src/main/java/com/hongikgrad/course/repository/UserCourseRepositoryCustom.java
@@ -1,9 +1,11 @@
 package com.hongikgrad.course.repository;
 
-import com.hongikgrad.course.dto.CourseResponseDto;
+import com.hongikgrad.course.dto.CourseDto;
+import com.hongikgrad.course.entity.Course;
 
 import java.util.List;
 
 public interface UserCourseRepositoryCustom {
-    List<CourseResponseDto> findUserTakenCoursesByStudentId(String studentId);
+    List<CourseDto> findUserTakenCoursesByStudentId(String studentId);
+    List<CourseDto> findUserTakenAbeekCoursesByStudentId(String studentId);
 }
