eb0695febd6a50d6c8b7c41f6b1d8af568aa4898
junhwan <ujunhwan@gmail.com>
2021-12-23 11:13:57 +0900

    [feat] 과목 찾는 쿼리 추가

--- src/main/java/com/hongikgrad/course/repository/CourseRepositoryCustom.java
+++ src/main/java/com/hongikgrad/course/repository/CourseRepositoryCustom.java
@@ -1,2 +1,12 @@
-package com.hongikgrad.course.repository;public interface CourseRepositoryCustom {
+package com.hongikgrad.course.repository;
+
+import com.hongikgrad.course.dto.CourseDto;
+
+import java.util.List;
+
+public interface CourseRepositoryCustom {
+	public List<CourseDto> findCoursesByAbeek(String abeek);
+	public List<CourseDto> findMajorEnglishCourses();
+	public List<CourseDto> findEnglishCourse();
+	public List<CourseDto> findWritingCourses();
 }
