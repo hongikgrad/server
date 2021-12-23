3ea50a1b514471290e477d25f4a75e1e02808de3
junhwan <ujunhwan@gmail.com>
2021-12-23 11:15:38 +0900

    [feat] 전공에 따른 필수전공 조회 쿼리 추가

--- src/main/java/com/hongikgrad/course/repository/MajorCourseCustom.java
+++ src/main/java/com/hongikgrad/course/repository/MajorCourseCustom.java
@@ -1,2 +1,11 @@
-package com.hongikgrad.course.repository;public interface MajorCourseCustom {
+package com.hongikgrad.course.repository;
+
+import com.hongikgrad.course.dto.CourseDto;
+import com.hongikgrad.course.entity.Major;
+
+import java.util.List;
+
+public interface MajorCourseCustom {
+	public List<CourseDto> findRequiredCoursesByMajor(Major studentMajor);
+	public List<CourseDto> findRequiredMSCCourse();
 }
--- src/main/java/com/hongikgrad/course/repository/MajorCourseRepository.java
+++ src/main/java/com/hongikgrad/course/repository/MajorCourseRepository.java
@@ -3,5 +3,5 @@ package com.hongikgrad.course.repository;
 import com.hongikgrad.course.entity.MajorCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MajorCourseRepository extends JpaRepository<MajorCourse, Long> {
+public interface MajorCourseRepository extends JpaRepository<MajorCourse, Long>, MajorCourseCustom {
 }
