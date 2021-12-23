1bc7c2e359b517722e59f34d3bf580797ed964d1
junhwan <ujunhwan@gmail.com>
2021-12-23 11:16:11 +0900

    [feat] 유저가 들은 과목이 저장되어있는지 확인하는 쿼리 추가

--- src/main/java/com/hongikgrad/course/repository/UserCourseRepositoryImpl.java
+++ src/main/java/com/hongikgrad/course/repository/UserCourseRepositoryImpl.java
@@ -1,14 +1,20 @@
 package com.hongikgrad.course.repository;
 
+import com.hongikgrad.authentication.entity.User;
 import com.hongikgrad.course.dto.CourseDto;
 import com.hongikgrad.course.dto.CourseResponseDto;
 import com.hongikgrad.course.entity.Course;
+import com.hongikgrad.course.entity.Major;
+import com.hongikgrad.course.entity.QMajor;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 import static com.hongikgrad.course.entity.QCourse.course;
+import static com.hongikgrad.course.entity.QMajor.*;
+import static com.hongikgrad.course.entity.QMajorCourse.majorCourse;
 import static com.hongikgrad.course.entity.QUserCourse.userCourse;
 import static com.querydsl.core.types.Projections.constructor;
 
@@ -45,5 +51,46 @@ public class UserCourseRepositoryImpl implements UserCourseRepositoryCustom {
                 .where(userCourse.user.studentId.eq(studentId).and(userCourse.course.abeek.length().gt(3)))
                 .fetch();
         return null;
+    }
+
+    @Override
+    public List<CourseDto> findUserTakenMajorCoursesByStudentId(String studentId, Major studentMajor) {
+        String generalMajorName = "";
+        if(studentMajor.getCollege().equals("공과대학")) {
+            generalMajorName = "공대교학과";
+        } else if(studentMajor.getCollege().equals("미술대학")) {
+            generalMajorName = "미대교학과";
+        } else if(studentMajor.getCollege().equals("사범대학")) {
+            generalMajorName = "사대교학과";
+        } else if(studentMajor.getCollege().equals("법과대학")) {
+            generalMajorName = "법과대 교학과";
+        }
+
+        Major generalMajor = queryFactory
+                .selectFrom(QMajor.major)
+                .where(QMajor.major.name.contains(generalMajorName))
+                .fetchOne();
+
+        return queryFactory
+                .select(
+                        constructor(
+                                CourseDto.class,
+                                userCourse.course.name,
+                                userCourse.course.number,
+                                userCourse.course.abeek,
+                                userCourse.course.credit
+                        ))
+                .from(userCourse)
+                .join(majorCourse).on(majorCourse.course.id.eq(userCourse.course.id).and(majorCourse.major.eq(studentMajor).or(majorCourse.major.eq(generalMajor))))
+                .where(userCourse.user.studentId.eq(studentId))
+                .fetch();
+    }
+
+    @Override
+    public boolean existsUserTakenCourse(User user, Course course) {
+        return queryFactory
+                .from(userCourse)
+                .where(userCourse.user.eq(user).and(userCourse.course.eq(course)))
+                .fetchFirst() != null;
     }
 }
