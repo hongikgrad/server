48aec65637dfb55152654f2955ed044eb074a59d
junhwan <ujunhwan@gmail.com>
2022-02-10 15:07:01 +0900

    [feat] url column 추가를 위한 dto 생성자 변경

--- src/main/java/com/hongikgrad/graduation/dto/SubField.java
+++ src/main/java/com/hongikgrad/graduation/dto/SubField.java
@@ -13,8 +13,20 @@ public class SubField {
 	private List<CourseDto> courseList;
 	private Integer totalCredit;
 	private boolean isSatisfied;
+	private String url;
+
+	public SubField(String field, List<CourseDto> courseList, Integer totalCredit, boolean isSatisfied) {
+		this.field = field;
+		this.courseList = courseList;
+		this.totalCredit = totalCredit;
+		this.isSatisfied = isSatisfied;
+		this.url = getCourseUrl("cat", field);
+	}
 
 	public void setTotalCredit(Integer totalCredit) {
 		this.totalCredit = totalCredit;
+	}
+	private String getCourseUrl(String command, String keyword) {
+		return "/courses?" + "command=" + command + "&keyword=" + keyword;
 	}
 }
