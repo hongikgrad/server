d85ab43c79c10fb05acf526bd96c67100b7cdaa6
junhwan <ujunhwan@gmail.com>
2022-02-14 02:26:28 +0900

    [feat] MajorCourse 존재 확인 쿼리 추가

--- src/main/java/com/hongikgrad/major/repository/MajorCourseRepository.java
+++ src/main/java/com/hongikgrad/major/repository/MajorCourseRepository.java
@@ -1,10 +1,11 @@
-package com.hongikgrad.course.repository;
+package com.hongikgrad.major.repository;
 
 import com.hongikgrad.course.entity.Course;
-import com.hongikgrad.course.entity.Major;
-import com.hongikgrad.course.entity.MajorCourse;
+import com.hongikgrad.major.entity.Major;
+import com.hongikgrad.major.entity.MajorCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MajorCourseRepository extends JpaRepository<MajorCourse, Long>, MajorCourseCustom {
+public interface MajorCourseRepository extends JpaRepository<MajorCourse, Long>, MajorCourseRepositoryCustom {
 	public MajorCourse findMajorCourseByCourseAndMajor(Course course, Major major);
+	public Boolean existsMajorCourseByMajorAndCourse(Major major, Course course);
 }
