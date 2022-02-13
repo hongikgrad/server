65f600dc26b35ad9a96421b36aa1c29794a66cbf
junhwan <ujunhwan@gmail.com>
2022-02-14 02:23:51 +0900

    [feat] 수강과목 처리방식 변경

--- src/main/java/com/hongikgrad/course/controller/UserCourseController.java
+++ src/main/java/com/hongikgrad/course/controller/UserCourseController.java
@@ -1,6 +1,7 @@
 package com.hongikgrad.course.controller;
 
 import com.hongikgrad.course.application.CourseService;
+import com.hongikgrad.course.dto.CourseDto;
 import com.hongikgrad.course.dto.UserTakenCourseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -19,33 +19,16 @@ public class UserCourseController {
 
     private final CourseService courseService;
 
-    /* 유저가 들은 수업들을 응답 */
-    @GetMapping("/users/courses")
-    public ResponseEntity getUserCourses(HttpServletRequest request) {
-        try {
-            return new ResponseEntity<>(
-                    courseService.getUserTakenCourses(request),
-                    HttpStatus.OK
-            );
-        } catch (NullPointerException e) {
-            return new ResponseEntity<>("유저의 정보를 알 수 없습니다.",
-                    HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    /* 유저가 들은 수업들 크롤링 한 뒤 데이터베이스에 저장 */
+    /* 유저가 들은 수업을 크롤링 한 뒤 응답 */
     @PostMapping("/users/courses")
-    public ResponseEntity<String> saveUserCourses(HttpServletRequest request) {
+    public ResponseEntity userCoursesPOST(HttpServletRequest request) {
         try {
-            courseService.saveUserTakenCourses(request);
-            return new ResponseEntity<>("저장 성공", HttpStatus.OK);
-        } catch (IOException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } catch(NullPointerException e) {
-            return new ResponseEntity<>("크롤링 실패", HttpStatus.BAD_REQUEST);
+            List<CourseDto> userTakenCourses = courseService.loadUserTakenCourses(request);
+            Integer totalCredit = courseService.getUserTakenTotalCredit(userTakenCourses);
+            Integer totalCount = userTakenCourses.size();
+            return new ResponseEntity(new UserTakenCourseDto(totalCredit, totalCount, userTakenCourses), HttpStatus.OK);
+        } catch(Exception e) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
     }
 }
