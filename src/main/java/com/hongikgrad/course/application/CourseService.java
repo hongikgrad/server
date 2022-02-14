9fa65679ad93117d30bfdc303a8fd77708a7cc3a
junhwan <ujunhwan@gmail.com>
2022-02-14 11:19:32 +0900

    [feat] user course crawling V2 생성 및 적용

--- src/main/java/com/hongikgrad/course/application/CourseService.java
+++ src/main/java/com/hongikgrad/course/application/CourseService.java
@@ -1,15 +1,15 @@
 package com.hongikgrad.course.application;
 
-import com.hongikgrad.authentication.entity.User;
 import com.hongikgrad.authentication.repository.UserRepository;
 import com.hongikgrad.common.application.CookieService;
 import com.hongikgrad.common.crawler.CourseCrawler;
 import com.hongikgrad.common.crawler.UserCourseCrawler;
 import com.hongikgrad.course.dto.*;
 import com.hongikgrad.course.entity.Course;
+import com.hongikgrad.course.exception.InvalidCookieException;
+import com.hongikgrad.course.exception.InvalidDocumentException;
 import com.hongikgrad.major.entity.Major;
 import com.hongikgrad.major.entity.MajorCourse;
-import com.hongikgrad.course.entity.UserCourse;
 import com.hongikgrad.course.repository.CourseRepository;
 import com.hongikgrad.major.repository.MajorCourseRepository;
 import com.hongikgrad.major.repository.MajorRepository;
@@ -19,7 +19,6 @@ import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -204,6 +203,8 @@ public class CourseService {
 		} catch (IndexOutOfBoundsException e) {
 			e.printStackTrace();
 //            return null;
+		} catch (InvalidDocumentException e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -212,12 +213,95 @@ public class CourseService {
 	}
 
 	/* 유저가 들은 수업들을 반환 */
-	public List<CourseDto> loadUserTakenCourses(HttpServletRequest request) throws IOException {
-		List<CourseDto> userTakenCourses = getUserTakenCoursesFromClassnet(request);
+	public List<CourseDto> loadUserTakenCourses(HttpServletRequest request) throws InvalidCookieException, InvalidDocumentException {
+		List<CourseDto> userTakenCourses = getUserTakenCoursesFromClassnetV2(request);
 		return userTakenCourses;
 	}
 
-	public List<CourseDto> getUserTakenCoursesFromClassnet(HttpServletRequest request) throws IOException, NullPointerException {
+	public List<CourseDto> getUserTakenCoursesFromClassnetV2(HttpServletRequest request) throws InvalidDocumentException, InvalidCookieException {
+		List<CourseDto> courseDtoList = new ArrayList<>();
+		Document userTakenCourseDocument = courseCrawler.getJsoupResponseDocument(TAKEN_COURSE_URL, extractCookie(request), courseCrawler.getHeaders(), null, Connection.Method.POST);
+		Element body = userTakenCourseDocument.getElementById("body");
+		Elements semesterTableList = getValidTableElements(body);
+
+		for (Element table : semesterTableList) {
+			Element tableBody = table.selectFirst("tbody");
+			if(tableBody == null) continue;
+			Elements tableRows = tableBody.children();
+			extractCourseData(tableRows, courseDtoList);
+		}
+		return courseDtoList;
+	}
+
+	private void extractCourseData(Elements tableRows, List<CourseDto> courseDtoList) {
+		try {
+			for (Element tableRow : tableRows) {
+				if (!isCourseInfoRow(tableRow)) continue;
+				Element courseNumberElement = tableRow.child(0);
+				Element courseCreditElement = tableRow.child(3);
+				String courseNumber = getTextFromElement(courseNumberElement);
+				int courseCredit = Integer.parseInt(getTextFromElement(courseCreditElement));
+				findAndSaveCourse(courseNumber, courseCredit, courseDtoList);
+			}
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void findAndSaveCourse(String courseNumber, int courseCredit, List<CourseDto> courseDtoList) {
+		if (courseNumber != null) {
+			CourseDto courseDto = courseRepository.findCourseDtoByNumberAndCredit(courseNumber, courseCredit);
+			if (courseDto == null) return;
+			courseDtoList.add(courseDto);
+		}
+	}
+
+	private String getTextFromElement(Element element) {
+		if(element != null) {
+			return element.text();
+		}
+		return "";
+	}
+
+	private Boolean isCourseInfoRow(Element tableRow) {
+		if(tableRow == null || tableRow.childrenSize() < 4) return false;
+		return true;
+	}
+
+	private Elements getValidTableElements(Element body) throws InvalidDocumentException {
+		validateBodyElement(body);
+		Elements bodyChildren = body.children();
+		filterTableChildren(bodyChildren);
+		return bodyChildren;
+	}
+
+	private void filterTableChildren(Elements elements) {
+//		for (Element bodyChild : elements) {
+//			if (hasElementChildTable(bodyChild)) continue;
+//			elements.removeIf(bodyChild);
+//		}
+		elements.removeIf(element -> !hasElementChildTable(element));
+	}
+
+	private void validateBodyElement(Element body) throws InvalidDocumentException {
+		if (body == null) {
+			throw new InvalidDocumentException("과목 페이지에서 body를 찾을 수 없습니다.");
+		}
+		Elements bodyChildren = body.children();
+		Integer bodyChildrenSize = body.childrenSize();
+		if (bodyChildren == null || bodyChildrenSize == null || bodyChildrenSize <= 2) {
+			throw new InvalidDocumentException("신입생이거나, 조회되는 수강 과목이 없습니다.");
+		}
+	}
+
+	private Boolean hasElementChildTable(Element element) {
+		if(element.children() != null
+				&& element.childrenSize() >= 1
+				&& element.children().first().is("table")) return true;
+		return false;
+	}
+
+	public List<CourseDto> getUserTakenCoursesFromClassnet(HttpServletRequest request) throws InvalidCookieException, InvalidDocumentException {
 		Document document = courseCrawler.getJsoupResponseDocument(TAKEN_COURSE_URL, extractCookie(request), courseCrawler.getHeaders(), null, Connection.Method.POST);
 		List<CourseDto> courseDtoList = new ArrayList<>();
 		Elements semesters = Objects.requireNonNull(document.getElementById("body")).select(".table0");
@@ -242,16 +326,21 @@ public class CourseService {
 		return courseDtoList;
 	}
 
-	public Map<String, String> extractCookie(HttpServletRequest request) {
-		Map<String, String> extractedCookies = new HashMap<>();
-		Cookie[] requestCookies = request.getCookies();
+	public Map<String, String> extractCookie(HttpServletRequest request) throws InvalidCookieException {
+		try {
+			Map<String, String> extractedCookies = new HashMap<>();
+			Cookie[] requestCookies = request.getCookies();
 
-		for (Cookie cookie : requestCookies) {
-			if (cookie == null) break;
-			extractedCookies.put(cookie.getName(), cookie.getValue());
+			for (Cookie cookie : requestCookies) {
+				if (cookie == null) break;
+				extractedCookies.put(cookie.getName(), cookie.getValue());
+			}
+
+			return extractedCookies;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new InvalidCookieException();
 		}
-
-		return extractedCookies;
 	}
 
 	private boolean checkProperTaken(Elements classInfo) {
