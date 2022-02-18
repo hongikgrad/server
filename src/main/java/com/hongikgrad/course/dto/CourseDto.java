00494aef0ffad2ed32db391990b5ff1dc165b13b
junhwan <ujunhwan@gmail.com>
2022-02-19 03:09:00 +0900

    [feat] coursedto: hashcode override 및 생성자 추가

--- src/main/java/com/hongikgrad/course/dto/CourseDto.java
+++ src/main/java/com/hongikgrad/course/dto/CourseDto.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CourseDto {
+	private Long id;
 	private String name;
 	private String number;
 	private String abeek;
@@ -30,9 +31,32 @@ public class CourseDto {
 		this.semester = semester;
 	}
 
+	public CourseDto(Long id, String name, String number, String abeek, int credit, String semester) {
+		this.id = id;
+		this.name = name;
+		this.number = number;
+		this.abeek = abeek;
+		this.credit = credit;
+		this.semester = semester;
+		this.isRequired = true;
+	}
+
+	public CourseDto(String name, String number, String abeek, int credit, boolean isRequired, String semester) {
+		this.name = name;
+		this.number = number;
+		this.abeek = abeek;
+		this.credit = credit;
+		this.isRequired = isRequired;
+		this.semester = semester;
+	}
+
 	@Override
 	public boolean equals(Object a) {
 		return (a instanceof CourseDto) && (((CourseDto) a).getNumber().equals(this.getNumber())) && ((CourseDto) a).getCredit() == this.getCredit();
 	}
 
+	@Override
+	public int hashCode() {
+		return (this.getNumber() + this.getCredit()).hashCode();
+	}
 }
