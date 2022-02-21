29b39c38a572e142cdc6fb6c63c72aabd97823de
junhwan <ujunhwan@gmail.com>
2022-02-21 17:55:49 +0900

    [feat] login v2 controller에 적용

--- src/main/java/com/hongikgrad/authentication/controller/UserController.java
+++ src/main/java/com/hongikgrad/authentication/controller/UserController.java
@@ -22,6 +22,34 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping(value = "/auth/token", produces = "application/json; charset=UTF-8")
+	public ResponseEntity loginV2(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+		try {
+			String adminID = System.getenv("ADMIN_ID");
+			String adminPW = System.getenv("ADMIN_PASSWORD");
+
+			if (loginRequestDto.getId().equals("test") && loginRequestDto.getPw().equals("1234")) {
+				userService.testLogin(response);
+			} else if (loginRequestDto.getId().equals(adminID) && loginRequestDto.getPw().equals(adminPW)) {
+				userService.adminLogin(response);
+			} else {
+				return new ResponseEntity<>(userService.loginV2(loginRequestDto), HttpStatus.OK);
+			}
+			return new ResponseEntity<>(HttpStatus.ACCEPTED);
+		} catch (AuthenticationException e) {
+			return new ResponseEntity<>(HttpStatus.NON_AUTHORITATIVE_INFORMATION);
+		} catch(NullPointerException e) {
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		} catch (IOException e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		} catch (NoSuchAlgorithmException e) {
+			return new ResponseEntity<String>("Invalid Student ID", HttpStatus.BAD_REQUEST);
+		} catch (InvalidDocumentException e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.NETWORK_AUTHENTICATION_REQUIRED);
+		}
+	}
+
+//	@PostMapping(value = "/auth/token", produces = "application/json; charset=UTF-8")
 	public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
 		try {
 			String adminID = System.getenv("ADMIN_ID");
